5ca3e8f470b5b4542545a06986afc3dfc4e94002
MohammadHossinzehi <mohammad.hossinzehi@ontariotechu.net>
2024-08-27 19:15:32 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function filesubmit() {
-    const repairType = document.getElementById('repairType').value;
-    const model = document.getElementById('model').value;
-    const price = document.getElementById('price').value;
+  const repairType = document.getElementById("repairType").value;
+  const model = document.getElementById("model").value;
+  const price = document.getElementById("price").value;
 
-    const currentDateTime = new Date().toLocaleString();
+  const currentDateTime = new Date().toLocaleString();
 
-    const textArea = document.querySelector('textarea');
-    const newEntry = `Repair: ${repairType}, Model: ${model}, Price: $${price}, Date: ${currentDateTime}\n`;
+  const textArea = document.querySelector("textarea");
+  const newEntry = `Repair: ${repairType}, Model: ${model}, Price: $${price}, Date: ${currentDateTime}\n`;
 
-    textArea.value += newEntry;
+  textArea.value += newEntry;
 
-    return false;
+  return false;
 }
